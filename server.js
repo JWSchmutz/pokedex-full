e285c2763dcf53da5c8b4568276435b3fa41221c
jwschmutz <jeremiah.schmutz@gmail.com>
2022-04-21 23:36:00 -0400

    production route

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(function (req, res, next) {
   next();
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.use("/", express.static(path.join(__dirname, "/client/build")));
 
 require("./config");
