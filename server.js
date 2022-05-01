f7f43bc46b6ae4e510397a6799dc43e1dadab64d
jwschmutz <jeremiah.schmutz@gmail.com>
2022-05-01 19:12:39 -0400

    back to original file structure

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
+require("dotenv").config();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
@@ -17,12 +18,13 @@ require("./config");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
-app.use("/", express.static(path.join(__dirname, "/client/build")));
-
 app.use("/", require("./routes"));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
