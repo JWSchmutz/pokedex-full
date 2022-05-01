136617e39135937bd00d6cbb07925e3c997a7dfe
jwschmutz <jeremiah.schmutz@gmail.com>
2022-05-01 17:51:25 -0400

    putting server foldeR

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+require("./config");
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
+app.use("/", express.static(path.join(__dirname, "/client/build")));
+
+app.use("/", require("./routes"));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
